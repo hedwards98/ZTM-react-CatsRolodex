882c24a184c7f217042bf0ce75e13702608ca3e5
hedwards98 <henry.edwards@mckesson.com>
2020-05-01 13:33:38 -0400

    adding files for github pages

--- src/App.js
+++ src/App.js
@@ -1,26 +1,51 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
+import { CardList } from './components/card-list/card-list.component';
 import './App.css';
+import { SearchBox } from './components/search-box/search-box.component'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+
+  constructor(){
+    super();
+    
+    this.state = {
+      cats: [],
+      searchField: ''
+    };
+  }
+
+  componentDidMount() {
+    fetch('https://jsonplaceholder.typicode.com/users')
+    .then(response => response.json())
+    .then(users => this.setState({ cats: users }));
+  }
+
+  // lexically scoped function, arrow function
+  handleChange = e => {
+    this.setState({ searchField: e.target.value })
+  }
+
+
+  render() {
+    const { cats,searchField } = this.state;
+    const filteredcats = cats.filter( cat =>
+      cat.name.toLowerCase().includes(searchField.toLowerCase())
+    );
+
+    return (
+      <div className='App'>
+        <h1>Cats Rolodex</h1>
+        <SearchBox
+          placeholder='search cats'
+          handleChange = { this.handleChange }
+        />
+
+        <CardList cats={filteredcats} />
+      </div>
+    );
+  }
+
+
 }
 
 export default App;
